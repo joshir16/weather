7f8cfbb366e0a077edb264e64cb7d606d6507118
Rishabh Joshi <joshir838@gmail.com>
2022-01-05 13:33:31 +0530

    current weather and next day weather | API change

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
+// current day------------------------------------------
 const day = document.querySelector(".day");
 const date = document.querySelector(".date");
 const month = document.querySelector(".month");
 const city = document.querySelector(".city");
 
-// -----------------------------------------------
+// current Temp------------------------------------------
 const temp = document.getElementById("temp");
 const msgType = document.getElementById("msg");
 const maxTemp = document.getElementById("max-temp");
 const minTemp = document.getElementById("min-temp");
 const windSpeed = document.getElementById("wind");
 const weatherImg = document.getElementById("weather__img");
+
+// tomorrow Temp------------------------------------------
+const dayNextMax = document.getElementById("daynext__max");
+const dayNextMin = document.getElementById("daynext__min");
+
+const dayNextDay = document.getElementById("daynext__day");
+const dayNextDate = document.getElementById("daynext__date");
+const dayNextMonth = document.getElementById("daynext__month");
+
+// tomorrow Temp------------------------------------------
+const dayAfterMax = document.getElementById("dayafter__max");
+const dayAfterMin = document.getElementById("dayafter__min");
+
+const dayAfterDay = document.getElementById("dayafter__day");
+const dayAfterDate = document.getElementById("dayafter__date");
+const dayAfterMonth = document.getElementById("dayafter__month");
 
 // ------------------------------------------------
 
@@ -23,40 +40,41 @@ const apiKey = "0fcceef01d62a49fc6701f61ed924699";
 /////////////////////////////////////////////////////////////////////////////
 
 const renderData = function (data) {
-  // console.log(data);
+  console.log(data);
 
-  temp.innerText = `${data.main.temp.toFixed(0)}`;
+  // current Temp
+  temp.innerText = `${data.current.temp.toFixed(1)}`;
 
-  msgType.innerText = data.weather[0].main;
-  const weatherMsg = data.weather[0].main.toLowerCase();
-
-  maxTemp.innerText = `${data.main.temp_max.toFixed(0)}`;
-  minTemp.innerText = `${data.main.temp_min.toFixed(0)}`;
-  humidity.innerText = `${data.main.humidity.toFixed(0)}`;
-  windSpeed.innerText = `${data.wind.speed.toFixed(0)}`;
-
+  msgType.innerText = data.current.weather[0].main;
+  const weatherMsg = data.current.weather[0].main.toLowerCase();
   weatherImg.classList.add(`${weatherMsg}`);
+
+  // today max and min temp
+  maxTemp.innerText = `${data.daily[0].temp.max.toFixed(1)}`;
+  minTemp.innerText = `${data.daily[0].temp.min.toFixed(1)}`;
+
+  humidity.innerText = `${data.current.humidity.toFixed(0)}`;
+  windSpeed.innerText = `${data.current.wind_speed.toFixed(1)}`;
+
+  // next day max and min temp
+  dayNextMax.innerText = `${data.daily[1].temp.max.toFixed(0)}`;
+  dayNextMin.innerText = `${data.daily[1].temp.min.toFixed(0)}`;
+
+  // day after max and min temp
+  dayAfterMax.innerText = `${data.daily[2].temp.max.toFixed(0)}`;
+  dayAfterMin.innerText = `${data.daily[2].temp.min.toFixed(0)}`;
+
+  // // dates ---------------------
+  // const dt = data.current.dt;
+  // console.log(dt);
 };
 
 /////////////////////////////////////////////////////////////////////////////
 
-// const request = new XMLHttpRequest();
-// request.open(
-//   "GET",
-//   `https://api.openweathermap.org/data/2.5/onecall?lat=28.5878598&lon=77.199748&exclude=minutely,hourly,alert&ppid=4c86492761337220a27c45b27aa1b770`
-// );
-// request.send();
-
-// document.addEventListener("load", function () {
-//   console.log(responseText);
-//   const [data] = JSON.parse(this.responseText);
-//   console.log(data);
-// });
-
-// // ----------------------------------------------------------
+// ----------------------------------------------------------
 const getWeatherData = function (lat, log) {
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&units=metric&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${log}&exclude=minutely,hourly,alerts&units=metric&appid=${apiKey}`
   )
     .then((response) => response.json())
     .then((data) => renderData(data));
